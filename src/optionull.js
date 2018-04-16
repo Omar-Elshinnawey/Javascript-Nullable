e92934aaabc4b7a195f1a7ab560c75868610babd
Omar El-Shinnawey <el-shinnaweyom@hotmail.com>
2018-04-16 21:02:38 +0300

    readme docs are complete. flatMap now always return optional

--- src/optionull.js
+++ src/optionull.js
@@ -1,112 +1,123 @@
-/**
- * @class
- */
-class Optional {
-	constructor(value) {
-		this.value = value;
-	}
+function Optional(value) {
+	this.value = value;
+}
 
+Optional.empty = function () {
+	return new Optional();
+}
+
+Optional.ofNullable = function(value) {
+	return new Optional(value);
+}
+
+Optional.prototype = {
 	/**
 	 * Returns the value (could be null or undefined).
 	 * @returns {any}
 	 */
-	get() {
+	get: function() {
 		return this.value;
-	}
+	},
 
 	/**
 	 * Returns the value if it is present otherwise returns the supplied defaultValue.
 	 * @param {any} defaultValue 
 	 * @returns {any}
 	 */
-	getOrElse(defaultValue) {
+	getOrElse: function(defaultValue) {
 		if (this.isPresent())
 			return this.value;
-
+		
 		if (typeof defaultValue === 'function')
 			return defaultValue();
 
 		return defaultValue;
-	}
+	},
 
 	/**
 	 * Returns true if the value is defined, false otherwise. (nulls return true).
 	 * @returns {boolean}
 	 */
-	isDefined() {
+	isDefined: function () {
 		if (typeof this.value === 'undefined')
 			return false;
 
 		return true;
-	}
+	},
 
 	/**
 	 * Returns true if the value is not null, false otherwise.
 	 * @returns {boolean}
 	 */
-	isNotNull() {
+	isNotNull: function() {
 		if (this.value === null)
 			return false;
-
+		
 		return true;
-	}
+	},
 
 	/**
 	 * Returns true if the value is not null and defined.
 	 * @returns {boolean}
 	 */
-	isPresent() {
-		if (this.isDefined() && this.isNotNull())
+	isPresent: function() {
+		if(this.isDefined() && this.isNotNull())
 			return true;
 
 		return false;
-	}
+	},
 
 	/**
 	 * Calls the function fn if the value is present.
 	 * @param {Function} fn 
 	 * @returns {void}
 	 */
-	ifPresent(fn) {
+	ifPresent: function(fn) {
 		if(this.isPresent())
 			fn(this.value);
-	}
+	},
 
 	/**
 	 * Calls the function fn if the value is present. otherwise calls elseFn.
 	 * @param {Function} fn 
 	 * @param {Function} elseFn 
 	 */
-	ifPresentOrElse(fn, elseFn) {
+	ifPresentOrElse: function(fn, elseFn) {
 		if(this.isPresent())
 			fn(this.value);
 		else
 			elseFn();
-	}
+	},
 
 	/**
 	 * Maps the value to another optional if the value exists.
 	 * @param {Function} fn 
 	 * @returns {Optional}
 	 */
-	map(fn) {
-		if (this.isPresent())
+	map: function(fn) {
+		if(this.isPresent())
 			return Optional.ofNullable(fn(this.value));
-
+		
 		return Optional.empty();
-	}
+	},
 
 	/**
 	 * Maps the value to another if the value exists. returns the new value or null.
 	 * @param {Function} fn 
 	 * @returns {any}
 	 */
-	flatMap(fn) {
-		if (this.isPresent())
-			return fn(this.value);
-		
-		return null;
-	}
+	flatMap: function(fn) {
+		if(this.isPresent()) {
+			const result = fn(this.value);
+
+			if (result instanceof Optional)
+				return result;
+			else
+				return Optional.ofNullable(result);
+		}
+
+		return Optional.empty();
+	},
 
 	/**
 	 * If the value is present and the result of predicate fn is true or fn is a value that is equal to the optional value a new optional is returned. 
@@ -114,31 +125,17 @@ class Optional {
 	 * @param {Function|any} fn 
 	 * @returns {Optional}
 	 */
-	filter(fn) {
-		if (this.isPresent()) {
-			const matches = typeof fn === 'function'? fn(this.value): this.value === fn;
+	filter: function(fn) {
+		if(this.isPresent()){
 
-			return matches? Optional.ofNullable(this.value): Optional.empty();
+			const matches = typeof fn === 'function'? fn(this.value): fn === this.value;
+
+			if(matches)
+				return Optional.ofNullable(this.value);
+
 		}
 
 		return Optional.empty();
-	}
-
-	/**
-	 * Returns an optional with empty value.
-	 * @returns {Optional}
-	 */
-	static empty() {
-		return new Optional();
-	}
-
-	/**
-	 * Returns an optional with the provided value.
-	 * @param {any} value 
-	 * @returns {Optional}
-	 */
-	static ofNullable(value) {
-		return new Optional(value);
 	}
 }
 
